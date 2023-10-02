ace316ea4c94708fb0ebd108c56670b0062336ec
catalin.stan <catalin.stan@softvision.com>
2023-10-02 17:29:20 +0300

    Added humidity in response

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,4 +1,6 @@
 const request = require("postman-request");
+
+
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=3c166e021a3f66c9ac2981638044c13b&query=' + latitude + ',' + longitude + '&units=m&'
 
@@ -8,7 +10,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location!', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.'
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like '
+                + body.current.feelslike + ' degrees out. The humidity is ' + body.current.humidity + '%.'
             )
         }
     })
